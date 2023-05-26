10297150d630878053aab7790bb310ebbd1da6ab
weijiaheng <jwei36@ucsc.edu>
2023-05-25 22:38:50 -0700

    reorg

--- data/datasets_cifarn.py
+++ data/datasets_cifarn.py
@@ -29,14 +29,14 @@ test_cifar100_transform = transforms.Compose([
 ])
 def input_dataset(dataset, noise_type, noise_path, is_human):
     if dataset == 'cifar10':
-        train_dataset = CIFAR10(root='~/data/',
+        train_dataset = CIFAR10(root='~/cifarn/data/',
                                 download=True,  
                                 train=True, 
                                 transform = train_cifar10_transform,
                                 noise_type = noise_type,
                                 noise_path = noise_path, is_human=is_human
                            )
-        test_dataset = CIFAR10(root='~/data/',
+        test_dataset = CIFAR10(root='~/cifarn/data/',
                                 download=False,  
                                 train=False, 
                                 transform = test_cifar10_transform,
